fa969dbf94688ab77bc89911e2c644f6fa186fd0
anth0nywong <87212104+anth0nywong@users.noreply.github.com>
2023-12-10 21:53:20 -0800

    verify api key endpoint.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from typing import Annotated
 from fastapi import FastAPI, Depends
 import server.models as models
 from server.database import engine
-from routers import auth, avatar, netstatus, data_access, overview
+from routers import auth, avatar, netstatus, data_access, overview, api_key
 import routers as router
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
@@ -27,6 +27,7 @@ app.include_router(auth.router)
 app.include_router(avatar.router)
 app.include_router(netstatus.router)
 app.include_router(overview.router)
+app.include_router(api_key.router)
 app.include_router(data_access.redditRouter)
 app.include_router(data_access.twitterRouter)
 app.include_router(data_access.testRouter)
